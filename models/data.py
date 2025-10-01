a8a3d30566ee34d2f4e6ecf8dc77aaf8d7486395
Eliana <valdezeliana38@gmail.com>
2025-10-01 04:13:56 -0300

    Add initial project files including Python version, app structure, data model, and dependencies

---
+++ models/data.py
@@ -0,0 +1,13 @@
+GATITOS = [
+    {"id": 1, "nombre": "Mishu", "edad": 2, "raza": "Siames", "color": "Blanco y marrón", "vacunado": True, "esterilizado": True},
+    {"id": 2, "nombre": "Luna", "edad": 1, "raza": "Persa", "color": "Gris", "vacunado": True, "esterilizado": False},
+    {"id": 3, "nombre": "Tomás", "edad": 4, "raza": "Mestizo", "color": "Negro", "vacunado": False, "esterilizado": False},
+    {"id": 4, "nombre": "Nube", "edad": 3, "raza": "Angora", "color": "Blanco", "vacunado": True, "esterilizado": True},
+    {"id": 5, "nombre": "Coco", "edad": 5, "raza": "Bengalí", "color": "Naranja con manchas", "vacunado": True, "esterilizado": True},
+    {"id": 6, "nombre": "Félix", "edad": 2, "raza": "Mestizo", "color": "Gris y blanco", "vacunado": False, "esterilizado": True},
+    {"id": 7, "nombre": "Canela", "edad": 3, "raza": "Siames", "color": "Marrón claro", "vacunado": True, "esterilizado": False},
+    {"id": 8, "nombre": "Bola", "edad": 1, "raza": "Persa", "color": "Negro y blanco", "vacunado": True, "esterilizado": True},
+    {"id": 9, "nombre": "Rayas", "edad": 4, "raza": "Bengalí", "color": "Naranja con rayas", "vacunado": False, "esterilizado": False},
+    {"id": 10, "nombre": "Pelusa", "edad": 2, "raza": "Angora", "color": "Blanco", "vacunado": True, "esterilizado": True},
+    {"id": 10, "nombre": "test", "edad": 3, "raza": "Siamés", "color": "Marrón y Cremita", "vacunado": False, "esterilizado": True},
+]
